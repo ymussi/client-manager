3f4221c59ea3b68607786c132328bbec0b29c400
Yuri Mussi <mussi@MacBook-Air-de-Yuri.local>
2020-07-13 15:37:37 -0300

    remove authorization to healthcheck

--- wishlist/api/healthcheck/view.py
+++ wishlist/api/healthcheck/view.py
@@ -9,14 +9,8 @@ from wishlist.api.healthcheck import get_first_record
 log = logging.getLogger(__name__)
 ns = api.namespace('healthcheck', description='Communication test.')
 
-parser_internal= ns.parser()
-
-parser = parser_internal.copy()
-parser.add_argument('Authorization', type=str, location='headers',
-                    help='Authorization Token to get access to Client Manager API', required=True)
 
 @ns.route('/')
-@ns.doc(parser=parser)
 class HealhCheck(Resource):
     @ns.response(code=400, description='Bad Request')
     def get(self):
